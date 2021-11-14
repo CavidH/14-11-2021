bac3788a50a8db55f26d99aed2245d235d98ece6
Cavid <hcavid386@gmail.com>
2021-11-14 22:11:19 +0400

    Update Program.cs

--- 14-11-2021/Program.cs
+++ 14-11-2021/Program.cs
@@ -22,20 +22,7 @@ namespace _14_11_2021
             
         }
 
-        private static void CopyFilesRecursively(string sourcePath, string targetPath)
-        {
-            //Now Create all of the directories
-            foreach (string dirPath in Directory.GetDirectories(sourcePath, "*", SearchOption.AllDirectories))
-            {
-                Directory.CreateDirectory(dirPath.Replace(sourcePath, targetPath));
-            }
-
-            //Copy all the files & Replaces any files with the same name
-            foreach (string newPath in Directory.GetFiles(sourcePath, "*.*", SearchOption.AllDirectories))
-            {
-                File.Copy(newPath, newPath.Replace(sourcePath, targetPath), true);
-            }
-        }
+        
 
     }
 }
