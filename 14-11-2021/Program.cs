500ee2d96a1ad10ed0a460ceff8a0dc942173a46
cavid <hcavid386@gmail.con>
2021-11-14 22:43:46 +0400

    ...

--- 14-11-2021/Program.cs
+++ 14-11-2021/Program.cs
@@ -5,25 +5,38 @@ namespace _14_11_2021
 {
     internal class Program
     {
-        
+
         static void Main(string[] args)
         {
             string DefaultPath = @"C:\Users\cavid\Desktop\code";
             string CopyPath = DefaultPath + "Copy";
-            // CopyFilesRecursively(DefaultPath, CopyPath);
-            CopyPath(new DirectoryInfo(DefaultPath), new DirectoryInfo(CopyPath));
+            CopyDirWithContent(new DirectoryInfo(DefaultPath), new DirectoryInfo(CopyPath));
 
 
 
         }
-
-        public static void CopyDirWithContent(DirectoryInfo DefaultPath,DirectoryInfo CopyPath)
+        public static void CopyDirWithContent(DirectoryInfo DefaultPath, DirectoryInfo CopyPath)
         {
-            
+
+
+
+            Console.WriteLine(CopyPath.FullName + " basha qayitdi");
+            Directory.CreateDirectory(CopyPath.FullName);
+
+            // yaratdiqmiz dir de olan fayli copy edirik
+            foreach (FileInfo file in DefaultPath.GetFiles())
+            {
+                Console.WriteLine(@"Copying {0}\{1}", CopyPath.FullName, file.Name);
+                file.CopyTo(Path.Combine(CopyPath.FullName, file.Name), true);
+            }
+
+          
+            foreach (DirectoryInfo DefaultPathDir in DefaultPath.GetDirectories())
+            {
+                DirectoryInfo SubDirector =
+                    CopyPath.CreateSubdirectory(DefaultPathDir.Name);
+                CopyDirWithContent(DefaultPathDir, SubDirector);
+            }
         }
-
-        
-
     }
 }
- 
